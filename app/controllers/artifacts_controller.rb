3d706c34db5ba3f2fb25637763453d80be370460
Ali Ibrahim <aibrahim2k2@gmail.com>
2014-01-25 19:40:02 -0500

    Add Artifact CRUD actions/views

--- app/controllers/artifacts_controller.rb
+++ app/controllers/artifacts_controller.rb
@@ -2,4 +2,36 @@ class ArtifactsController < ApplicationController
   def index
     @artifacts = Artifact.all
   end
+
+  def show
+    @artifact = Artifact.find params[:id]
+  end
+
+  def edit
+    @artifact = Artifact.find params[:id]
+  end
+
+  def update
+    @artifact = Artifact.find params[:id]
+
+    if @artifact.update_attributes artifact_params
+      redirect_to action: :index
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def artifact_params
+    params.require(:artifact).permit(
+      :title,
+      :description,
+      :artist,
+      :date,
+      :history,
+      :references,
+      :location
+    )
+  end
 end
